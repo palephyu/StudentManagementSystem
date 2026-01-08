9add5fb7e7e333a41a4f043692b642527eceaba8
palephyu <palephyu12345@gmail.com>
2026-01-08 23:04:53 +0630

    update studentapi in mvc at11:04Pm 8jan2026

---
+++ Controllers/StudentAPIController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Models;
+using System;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentAPIController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string apiBaseUrl = "https://localhost:7097/api/Student";
+        public StudentAPIController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var response = await _httpClient.GetFromJsonAsync<ApiResponse<List<StudentTb>>>(apiBaseUrl);
+            //var students = response.Data;
+            //return View(students);
+            List<StudentTb> students = new List<StudentTb>();
+            var response =  await _httpClient.GetAsync(apiBaseUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadFromJsonAsync<ApiResponse<List<StudentTb>>>();
+                students = apiResponse?.Data;
+            }
+            return View(students);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(StudentTb student)
+        {
+            var response = await _httpClient.PostAsJsonAsync(
+                apiBaseUrl, student);
+
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError("", "Insert failed");
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, StudentTb student)
+        {
+            var response = await _httpClient.PutAsJsonAsync(
+                $"https://localhost:7097/api/Student/{id}", student);
+
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            return View(student);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _httpClient.DeleteAsync($"https://localhost:7097/api/Student/{id}");
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
